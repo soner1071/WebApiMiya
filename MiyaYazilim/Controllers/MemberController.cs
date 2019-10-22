215c98670c86e7f636cb8f662a984452c6dbabd8
soner1071 <sonerrakarsu@gmail.com>
2019-10-23 02:29:52 +0300

    mülakat

---
+++ MiyaYazilim/Controllers/MemberController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+using System.Web.Script.Serialization;
+using MiyaYazilim.Models;
+
+namespace MiyaYazilim.Controllers
+{
+    public class MemberController : Controller
+    {
+        // GET: Member
+        public ActionResult List()
+        {
+            ViewBag.Data = GetApiData();
+            return View();
+        }
+
+        public List<PageInfo> GetApiData()
+        {
+
+            var apiUrl = "https://reqres.in/api/users?page=2";
+
+            //Connect API
+            Uri url = new Uri(apiUrl);
+            WebClient client = new WebClient();
+            client.Encoding = System.Text.Encoding.UTF8;
+
+            string json = client.DownloadString(url);
+            //END
+
+            //JSON Parse START
+            JavaScriptSerializer ser = new JavaScriptSerializer();
+            List<PageInfo> jsonList = ser.Deserialize<List<PageInfo>>(json);
+            PageInfo PI = ser.Deserialize<PageInfo>(json);
+            ViewBag.listem = PI.data.ToList();
+            
+            //END
+
+            return jsonList;
+        }
+
+
+
+        [HttpGet]
+        public ActionResult Add()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(DataInfo model)  
+        {
+           
+
+            DataInfo yeni = new DataInfo();
+            yeni.first_name = model.first_name;
+            yeni.last_name = model.last_name;
+            
+
+
+            return RedirectToAction("List");
+
+
+        }
+    }
+}
---
+++ MiyaYazilim/Models/ApiModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MiyaYazilim.Models
+{
+    public class DataInfo
+    {
+        public int id { get; set; }
+        public string email { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string avatar { get; set; }
+    }
+
+    public class PageInfo
+    {
+        public int page { get; set; }
+        public int per_page { get; set; }
+        public int total { get; set; }
+        public int total_pages { get; set; }
+        public List<DataInfo> data { get; set; }
+    }
+}
